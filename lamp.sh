adf1221fb97b09ae9baaf309cc887dd35ed50bdd
Kennashka DeSilva <kennashka@yahoo.com>
2018-12-15 22:39:22 -0600

    Create lamp.sh

---
+++ lamp.sh
@@ -0,0 +1,4 @@
+sudo apt-get install apache2 -y
+sudo apt-get install php -y
+sudo apt install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php -y
+sudo apt install mysql-server -y
